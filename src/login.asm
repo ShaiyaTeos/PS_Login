98d7e5d4af515b12bbef87d78349b8447cd477d2
Triston Plummer <me@triston.io>
2021-09-29 02:47:15 +0800

    feat(Login): Add support for the Shaiya US OAuth packet.
    While this doesn't implement OAuth, the login server now parses a custom
    input token and redirects it to the login function in a known-good
    format.
    
    Starting the Shaiya US client as `./game start 127.0.0.1
    username/password/` logs in successfully.

--- src/login.asm
+++ src/login.asm
@@ -7,11 +7,16 @@ va_section  .text
 ; Teos PS_Login (built on top of the original Ep4 ps_login found at https://archive.shaiya.net/server/ep4.5/PSM_Client/Bin/ps_login.exe)
 ; ==============================================================================================================================================================
 
+; Patch: Add support for handling custom packets.
+va_org      0x40436D
+jmp         CUser_OnRecv
+times       2 nop
+
 ; Patch: Fix invalid RSA payload crash (https://www.elitepvpers.com/forum/shaiya-pserver-guides-releases/4387243-ps_login-handshake-exploit-fix.html#post36452650)
 va_org      0x404542
 jne         0x4044BE
 
-; Patch: Fix the call to `try_gamelogin_korea`.
+; Patch: Fix the call to `try_gamelogin_korea`, and the sql injection.
 va_org      0x406ED9
 push        GameLogin_Exec
 
@@ -24,6 +29,7 @@ va_section  .data
 ; Include custom code.
 va_section  .teos
 %include    "src/sql.asm"
+%include    "src/packets.asm"
 
 ; Include the remaining data.
 va_org      end
